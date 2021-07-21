de92832e4dea06df675830b4e352cf8249c62978
DunZek <duncanrledesma_drl@yahoo.com.ph>
2021-07-21 08:22:48 -0600

    initialized

--- 10_modules/main.js
+++ 10_modules/main.js
@@ -1,1 +1,17 @@
 // "Write code that is easy to delete, not easy to extend." - Tef, Programming is Terrible
+
+/* Modules */
+
+/* Packages */
+
+/* Improvised modules */
+
+/* Evaluating data as code */
+
+/* CommonJS */
+
+/* EcmaScript Modules */
+
+/* Building and bundling */
+
+/* Module design */
