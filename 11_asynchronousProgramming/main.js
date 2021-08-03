0464dee5ffbeac42ebc183e6d61cc6c4126ba426
DunZek <duncanrledesma_drl@yahoo.com.ph>
2021-08-03 10:04:36 -0600

    update

--- 11_asynchronousProgramming/main.js
+++ 11_asynchronousProgramming/main.js
@@ -2,7 +2,23 @@
 
 /* Crow Tech */
 
-/* Callbacks */
+/* Callbacks
+    - A way to tackle asynchronous programming is to use "callback functions" for actions that take long.
+    - when the action is finished, the callback function is called with the result
+*/
+// Example - using a callback function with setTimeout()
+setTimeout(() => console.log("Tick"), 500)
+
+// Looking up a food cache in the storage bubls of Big Oak
+import {bigOak} from './crow-tech'
+bigOak.readStorage("food caches", caches => {
+    let firstCache = caches[0]
+    bigOak.readStorage(firstCache, info => {
+        console.log(info)
+    })
+})
+
+// ./crow-tech.send(target_nest_name, type_of_request, content, callback_function)
 
 /* Promises */
 
