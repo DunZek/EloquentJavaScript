58ad975db6b6d4d39da515c572d0bdbee9d5d559
DunZek <60448983+DunZek@users.noreply.github.com>
2022-04-01 14:27:16 -0600

    2022-04-01 2:27 PM refactored notes

--- part1_language/1_valuesTypesOperators/main.js
+++ part1_language/1_valuesTypesOperators/main.js
@@ -1,64 +1,105 @@
-/*
-    Numbers 
-    - 64 bits (2^64) can be used to represent a number (signed integer, float).
+// Execute examples:
+
+
+
+// Notes below, as they are ordered in the book:
+
+/*  Numbers:
     - Actual maximum whole number to be represented is around 9 quadrillion
+    - 64 bits (2^64) can be used to represent a number (signed integer, float).
 */
 
-/* Special numbers */
-positive_infinity = Infinity  // the converted value of a positive number that is too big
-negative_infinity = -Infinity  // for negative numbers that are too big
-console.log(0 == NaN == Infinity - Infinity)  // NaN's are used to denote the value of nonsensical operations
+function special_numbers() {
+    // Positive numbers too big to represent evaluate to Infinity:
+    positive_infinity = Infinity;
+    // Negative numbers too big to represent evaluate to -Infinity:
+    negative_infinity = -Infinity;
+    // NaN's are used to denote the value of nonsensical operations:
+    console.log(0 == NaN == Infinity - Infinity);
+}
 
-/*
-    Strings
+/*  Strings:
     - 16 bits (2^16) can be used to represent a string. It is the maximum size.
     - There are over 2^16 possible unicode characters.
 */
-"Lie on " + 'the ocean floor'  // single and double are interchangeable to represent a string
-// Back ticks carry over white spaces unlike quotation strings
-tick = `Hello
-World`
-console.log(tick)
+function strings() {
+    // Single and double quotes are interchangeable to represent a string:
+    "Lie on " + 'the ocean floor';
+    // Back ticks carry over white spaces unlike quotation strings:
+    tick = `Hello
+    World`;
+    // console.log() prints strings:
+    console.log(tick);
+}
 
-9.81  // fractional numbers
-2.998e8  // 2.998e8 = 2.998 x 10^8 <- scientific notation
+function more_numbers() {
+    // Fractional numbers:
+    console.log(9.81);
+    // Scientific notation:
+    console.log(2.998e8);  // 2.998e8 = 2.998 x 10^8
+}
 
-/* Unary operators */
-console.log(-50)
-console.log(!(true == true))  // ... false
-console.log(typeof 30)  // ... number
+function unary_operators() {
+    // Negation operators:
+    console.log(-50);
+    console.log(!(true == true));  // false
+    // Getting the type of a value:
+    console.log(typeof 30);  // number
+}
 
-/* Comparison */
-console.log("apple" > "Achimedes")  // ... true <- The ordinal unicode position of the first character is compared to one another
+function comparison() {
+    // The ordinal unicode position of the characters can be compared to one another:
+    console.log("apple" > "Achimedes");  // true
+    // NaN denotes nonsensical computation and isn't equal to anything, even itself:
+    console.log(NaN === NaN);  // false
+}
 
-console.log(NaN == NaN)  // ... false -> NaN denotes nonsensical computation and isn't equal to any other NaN
+/*  Logical operators:
+    The order of operators in JavaScript go as follows:
+    - First (Arithmetic): +, -, *, /, %
+    - Second (Comparisons): >=, ==
+    - Third: &&
+    - Fourth: ||
+*/
+function logical_operators() {
+    // Example:
+    console.log("Example: ", 1 + 1 == 2 && 10 * 10 > 50);  // true  
+}
 
-/* Logical operators */
-console.log(1 + 1 == 2 && 10 * 10 > 50)  // ... true
-// +, %, // <- first
-// >=, ==  <- second
-// AND <- third
-// OR <- last
+function ternary_operator() {
+    // Reads as: "if true, then return left value, else right"
+    console.log(true ? 1 : 2);  // 1
+    console.log(false ? 1 : 2);  // 2
+}
 
-console.log(true ? 1 : 2)  // ... 1 <- if true, then return left value, else right
-console.log(false ? 1 : 2)  // ... 2
+function empty_values() {
+    // "undefined" denotes the absence of a meaningful value. 
+    // They are often used as return placeholders.
+    console.log(null == undefined);
+}
 
-/* Empty values */
-console.log(null == undefined)  // denote the absence of a meaningful value. Can be used as return placeholders.
+function automatic_type_conversion () {
+    // JavaScript attempts to convert incompatible types and execute the operation:
+    console.log(8 * null);      // 8 * 0
+    console.log("5" - 1);       // 5 - 1
+    console.log("5" + 1);       // "5" + "1"
+    console.log("five" * 2);    // NaN
+    console.log(false == 0);    // 0 == 0
+    // Use the identical comparison operator to negate type conversion:
+    console.log(false === 0);   // false
+    // It is very useful.
+}
 
-/* Automatic type conversion - JavaScript attempting to convert incompatible types and execute the operation */
-console.log(8 * null)  // null -> 0 ... 0
-console.log("5" - 1)  // "5" -> 5 ... 4
-console.log("5" + 1)  // 1 -> "1" ... "51"
-console.log("five" * 2)  // ... NaN
-console.log(false == 0)  // false -> 0 ... true
-
-console.log(false === 0)  // ... false <- use the identical comparison operator to negate type conversion. Very useful.
-
-/* Short-circuiting of logical operators - used to control the evaluation of type-converting logical operators */
-// The type-conversion of logical operators is as defined as below
-console.log(null || "user")  // ... "user" <- OR returns left value if it can be type-converted to true, otherwise the right
-console.log("Agnes" || "cannot be converted to 'true'")  // ... "Agnes"
-console.log(0 || false, NaN || false, "" || false)  // ... false, false, false <- everything else otherwise that isn't one of the left values can be converted to true
-
-console.log(null && "user")  // ... null <- AND returns left value if it can be type-converted to false, otherwise the right
+/*  Short-circuiting of logical operators:
+    - Used to control the evaluation of type-converting logical operators.
+    - The type-conversion of logical operators is as defined as below.
+*/
+function short_circuiting_of_logical_operators() {
+    // OR (||) returns the left-value if it can be type-converted to true, otherwise the right one:
+    console.log(null || "user")  // "user"
+    console.log("Agnes" || "cannot be converted to 'true'")  // "Agnes"
+    // Everything else otherwise that isn't one of the left values can be converted to true:
+    console.log(0 || false, NaN || false, "" || false)  // false, false, false
+    // AND returns the left-value if it can be type-converted to false, otherwise the right one:
+    console.log(null && "user")  // null
+}    
